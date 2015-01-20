39eb0687edfc92051e2b3ae0347aa67428ff9333
Charlie gerard <annecharlotte.gerard@gmail.com>
2015-01-20 23:18:57 +1100

    Update code to fit new version of framework and API

--- my_modules/myo_sphero/index.js
+++ my_modules/myo_sphero/index.js
@@ -1,10 +1,9 @@
 module.exports = function() {
 
-  // var Leap = require('leapjs');
   var spheron = require('spheron');
   var myo = require('myo');
 
-  myo = Myo.create(0);
+  myo = Myo.create();
 
   myo.on('arm_recognized', function(){
     console.log('good!', this.id);
@@ -14,54 +13,46 @@ module.exports = function() {
     console.log('arm lost', this.id);
   })
 
-  // ------------------------------
-  // Need to test these 2 gestures
-  // ------------------------------
-  // myo.on('double_tap', function(){
-  //   this.zeroOrientation();
-  //   console.log('double tap') ;
-  // });
-
-  // myo.on('gyroscope', function(data){
-  //   if(data.y < -2) console.log('hit!');
-  // });
-
   // Set this to the device Sphero connects as on your computer.
   var device = '/dev/tty.Sphero-RBR-AMP-SPP';
 
-  var safeMode = true; //Turn this off if Sphero is in water!
+  var safeMode = true;
 
   var controlSphero = function(sphero) {
     myo.on('wave_out', function(){
+      myo.setLockingPolicy();
       console.log('RIGHT');
       sphero.heading = 90;
       sphero.roll(70, 90, 1);
     })
 
     myo.on('wave_in', function(){
+      myo.setLockingPolicy();
       console.log('LEFT!');
       sphero.roll(70, 270, 1); //Heading is expressed in degrees so 270 will make the ball move to the left.
     })
 
     myo.on('fist', function(){
+      myo.setLockingPolicy();
       console.log('FORWARD');
-      myo.requestBluetoothStrength();
       sphero.roll(70, 0, 1);
     })
 
     myo.on('fingers_spread', function(edge){
-      console.log('BACKWARD');
-      myo.requestBluetoothStrength();
-      sphero.roll(70, 180, 1);
+      myo.setLockingPolicy();
+      // console.log('BACKWARD');
+      // sphero.roll(70, 180, 1);
+      ball.setRGB(spheron.toolbelt.COLORS.WHITE).setBackLED(100);
+      stopSphero(sphero);
     })
 
-    myo.on('rest', function(){
-      console.log('STOP');
-      if(!edge) return;
-      // myo.vibrate();
-      stopSphero(sphero);
-      ball.setRGB(spheron.toolbelt.COLORS.WHITE).setBackLED(255);
-    })
+    // myo.on('rest', function(){
+    //   console.log('STOP');
+    //   // if(!edge) return;
+    //   // myo.vibrate();
+    //   stopSphero(sphero);
+    //   ball.setRGB(spheron.toolbelt.COLORS.WHITE).setBackLED(255);
+    // })
   };
 
   var stopSphero = function(sphero) {
@@ -73,7 +64,7 @@ module.exports = function() {
 
   console.log("waiting for Sphero connection...");
   ball.on('open', function() {
-  	console.log('connected to Sphero');
+    console.log('connected to Sphero');
       ball.setRGB(spheron.toolbelt.COLORS.PURPLE).setBackLED(255);
       controlSphero(ball);
   });
