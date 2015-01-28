26c62117e67a9d942e2f1d3025e4afd97eb6c31c
Charlie gerard <annecharlotte.gerard@gmail.com>
2015-01-28 19:35:57 +1100

    Remove public folder

--- app.js
+++ app.js
@@ -10,12 +10,11 @@ var lab = require('./my_modules/myo_sphero');
 lab();
 
 // Will render something in the browser later
-// app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static(path.join(__dirname, 'views')));
 
-app.all('*', function(req, res){
-  res.sendfile('views/index.html');
-});
+// app.all('*', function(req, res){
+//   res.sendfile('views/index.html');
+// });
 
 app.listen(3001);
 console.log("Server running on port 3001");
