02b876c1ecc56fae5be7e7520945f7532133b3f0
Charlie gerard <annecharlotte.gerard@gmail.com>
2015-02-05 20:23:06 +1100

    Clean up code

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 var express = require('express'),
     path = require('path');
 
-//create web server
 var app = express();
 
-
-//require the custom module
 var lab = require('./my_modules/myo_sphero');
 lab();
-
-// Will render something in the browser later
-// app.use(express.static(path.join(__dirname, 'views')));
-
-// app.all('*', function(req, res){
-//   res.sendfile('views/index.html');
-// });
 
 app.listen(3001);
 console.log("Server running on port 3001");
