7bc45d4ddb57d7c73e91260a64edca9721c75ffd
Charlie Gerard <charlie.a.gerard@gmail.com>
2018-04-04 20:26:05 +1000

    update modules and remove unused code

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-var express = require('express'),
-    path = require('path');
-
-var app = express();
-
 var lab = require('./my_modules/myo_sphero');
 lab();
-
-app.listen(3001);
-console.log("Server running on port 3001");
 
 
 
--- my_modules/myo_sphero/index.js
+++ my_modules/myo_sphero/index.js
@@ -1,58 +1,49 @@
 module.exports = function() {
-
-  var sphero = require("sphero");
-  var myo = require('myo');
-
-  // myo = Myo.create();
+  // modified local copy of module because official one
+  // is not working with latest version of serialport.
+  const sphero = require("../sphero");
+  const myo = require('myo');
 
   myo.connect('com.charliegerard.test', require('ws'));
 
-  myo.on('arm_recognized', function(){
-    console.log('good!', this.id);
-  })
+  myo.on('arm_recognized', () => console.log('good!', this.id));
 
-  myo.on('arm_lost', function(){
-    console.log('arm lost', this.id);
-  })
+  myo.on('arm_lost', () => console.log('arm lost', this.id));
 
   // Set this to the port of your Sphero on your computer.
-  var device = sphero('/dev/tty.Sphero-RBR-AMP-SPP-1');
-
-  var safeMode = true;
+  const device = sphero('/dev/tty.Sphero-RBR-AMP-SPP');
 
   // myo.unlock();
 
-  var controlSphero = function(spheroBall) {
-    myo.on('wave_out', function(){
+  const controlSphero = spheroBall => {
+    myo.on('wave_out', () => {
       console.log('RIGHT');
       spheroBall.heading = 90;
       spheroBall.roll(70, 90, 1);
     })
 
-    myo.on('wave_in', function(){
+    myo.on('wave_in', () => {
       console.log('LEFT!');
       spheroBall.roll(70, 270, 1);
     })
 
-    myo.on('fist', function(){
+    myo.on('fist', () => {
       console.log('FORWARD');
       spheroBall.roll(70, 0, 1);
     })
 
-    myo.on('fingers_spread', function(edge){
+    myo.on('fingers_spread', edge => {
       stopSphero(spheroBall);
     })
 
   };
 
-  var stopSphero = function(spheroBall) {
-    spheroBall.roll(0,spheroBall.heading||0,0);
-  };
+  const stopSphero = spheroBall => spheroBall.roll(0,spheroBall.heading||0,0);
 
   console.log("waiting for Sphero connection...");
 
-  device.connect(function() {
+  device.connect(() => {
     console.log('connected to Sphero');
-      controlSphero(device);
+    controlSphero(device);
   });
 };
